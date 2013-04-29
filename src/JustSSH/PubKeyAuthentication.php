061f769d968391801f333a5331b135c411b8ea66
David Keijser <keijser@gmail.com>
2013-04-29 10:10:48 +0200

    make passphrase optional for pubkey authentication

--- src/JustSSH/PubKeyAuthentication.php
+++ src/JustSSH/PubKeyAuthentication.php
@@ -17,9 +17,11 @@ class PubKeyAuthentication implements AuthenticationInterface
     public function __construct($opts)
     {
         $this->username = $opts['username'];
-        $this->privkey = $opts['privkey'];
-        $this->pubkey = $opts['pubkey'];
-        $this->passphrase = $opts['passphrase'];
+        $this->privkey = realpath($opts['privkey']);
+        $this->pubkey = realpath($opts['pubkey']);
+        if (isset($opts['passphrase'])) {
+            $this->passphrase = $opts['passphrase'];
+        }
     }
 
     /**
@@ -32,13 +34,13 @@ class PubKeyAuthentication implements AuthenticationInterface
      */
     public function apply($session)
     {
-        $auth = ssh2_auth_pubkey_file(
-            $session,
-            $this->username,
-            realpath($this->pubkey),
-            realpath($this->privkey),
-            $this->passphrase
-        );
+        $args = array($session, $this->username, $this->pubkey, $this->privkey);
+
+        if ($this->passphrase) {
+            $args[] = $this->passphrase;
+        }
+
+        $auth = call_user_func_array('ssh2_auth_pubkey_file', $args);
 
         if (!$auth) {
             throw new \RuntimeException("Could not auth");
